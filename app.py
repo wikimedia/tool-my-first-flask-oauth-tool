ced7f0e047d477f9f0b58e97a12c1077029db4cb
Bryan Davis <bd808@wikimedia.org>
2017-02-03 18:19:14 -0700

    Add OAuth authentication using mwoauth
    Use the excellent mwoauth library to add support for OAuth
    authentication with the Wikimedia wikis.
    
    Adds three new configuration values that should be set in the local
    config.yaml:
    
    * SECRET_KEY: the Flask secret value used for session encryption
    * CONSUMER_KEY: OAuth consumer key
    * CONSUMER_SECRET: OAuth consumer secret

--- app.py
+++ app.py
@@ -16,13 +16,18 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Example Flask application which can authenticate the user via OAuth."""
 
 import flask
+import mwoauth
 import os
 import yaml
 
+
 app = flask.Flask(__name__)
 
+
+# Load configuration from YAML file(s)
 __dir__ = os.path.dirname(__file__)
 app.config.update(
     yaml.load(open(os.path.join(__dir__, 'default_config.yaml'))))
@@ -36,4 +41,69 @@ except IOError:
 
 @app.route('/')
 def index():
-  return app.config['GREETING']
+    """Application landing page."""
+    username = flask.session.get('username', None)
+    return flask.render_template('index.html', username=username)
+
+
+@app.route('/login')
+def login():
+    """Initiate an OAuth login.
+
+    Call the MediaWiki server to get request secrets and then redirect the
+    user to the MediaWiki server to sign the request.
+    """
+    consumer_token = mwoauth.ConsumerToken(
+        app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+    try:
+        redirect, request_token = mwoauth.initiate(
+            app.config['OAUTH_MWURI'], consumer_token)
+    except Exception:
+        app.logger.exception('mwoauth.initiate failed')
+        flask.flash(u'OAuth handshake failed.')
+        flask.redirect(flask.url_for('index'))
+    else:
+        flask.session['request_token'] = dict(zip(
+            request_token._fields, request_token))
+        return flask.redirect(redirect)
+
+
+@app.route('/oauth-callback')
+def oauth_callback():
+    """OAuth handshake callback."""
+    if 'request_token' not in flask.session:
+        flask.flash(u'OAuth callback failed. Are cookies disabled?')
+        return flask.redirect(flask.url_for('index'))
+
+    consumer_token = mwoauth.ConsumerToken(
+        app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+
+    try:
+        access_token = mwoauth.complete(
+            app.config['OAUTH_MWURI'],
+            consumer_token,
+            mwoauth.RequestToken(**flask.session['request_token']),
+            flask.request.query_string)
+
+        identity = mwoauth.identify(
+                app.config['OAUTH_MWURI'], consumer_token, access_token)
+
+    except Exception:
+        app.logger.exception('OAuth autnetication failed')
+        flask.flask(u'OAuth authentication failed.')
+
+    else:
+        flask.session['access_token'] = dict(zip(
+            access_token._fields, access_token))
+        flask.session['username'] = identity['username']
+        flask.flash(u'You were signed in, %s!' % identity['username'])
+
+    return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    """Log the user out by clearing their session."""
+    flask.session.clear()
+    flask.flash(u'You have been logged out.')
+    return flask.redirect(flask.url_for('index'))
